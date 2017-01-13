143d7e1ad3ddcea0da077002912730dee225af78
vivanfr <myhuong.nhan2502@gmail.com>
2017-01-13 11:55:04 +0100

    run script in daemon mode

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # rasp_temp
 Rasperry
+Run Python script in backgroud
+#copy the serive to systemd
+
+$ sudo cp thermo.service /lib/systemd/system
+
+#Enable service
+
+$ sudo systemctl daemon-reload
+$ sudo systemctl enable thermo.service
+$ sudo systemctl start thermo.servie
+
+# stop service
+$ sudo systemctl stop thermo.service
+
+# verify status of service
+
+$ sudo systemctl status thermo.service
+
+OR
+
+$ tail -f /var/log/syslog
--- temperature.py
+++ temperature.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 
-from os import system
+from os import system, path			
 from time import sleep, strftime
-
-
-
+import sys
 
 system('modprobe w1-gpio')
 system('modprobe w1-therm')
@@ -13,38 +11,45 @@ system('modprobe w1-therm')
 dir_temp = "/sys/bus/w1/devices/w1_bus_master1/28-031661fa43ff/w1_slave"
 
 #write temperature history 
-t_file = open('result.txt', 'w')
+t_file = open(path.join('/home/pi/Working','result.txt'), 'w')
 
 
-# read 
+# read temperature
 def read_file(file):
     f = open(file, 'r')
     lines = f.readlines()
     f.close()
     return lines
-
+	
+#Write temperature to text file
 def wtofile(value):
+
 	if t_file:
 		t_file.write(value)
 	else:
 		print("need to create a new file")
-
+	#write to file in real-time	
+	sys.stdout.flush()
+	t_file.flush()
 
 def convert_temp(temp):	
+
 	temp_raw = temp.split("=")[1]	
-	temp_final = round(int(temp_raw) / 1000)	
+	temp_final = round(int(temp_raw) / 1000, 2)	
 	return temp_final
 
 	
 if __name__ == '__main__':
 
 	while True:
+		
 		lines = read_file(dir_temp)		
 		temp = convert_temp(lines[1])
+		
 		# write ambient temperature every hours
 		if strftime("%M:%S")== "00:00":
-				value = strftime("%A, %H:%M")+ " Temperature : "+ str(temp)+"\n"
-		 
-				wtofile(value)
+			
+			val = strftime("%A, %H:%M:%S")+ " Temperature : "+ str(temp)+"\n"		 
+			wtofile(val)
 		
-		print (strftime("%A, %H:%M:%S"),"temperature","=",temp) # affichage a l'ecran		
+		#print (strftime("%A, %H:%M:%S"),"temperature","=",temp) # affichage a l'ecran		
